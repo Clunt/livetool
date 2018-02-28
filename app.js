fbfa44292af2420a3b67c6cec91973ac9a157bfa
Clunt <clunt@foxmail.com>
2018-03-01 00:24:48 +0800

    code

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/music', music);
+app.use('/camera', (req, res) => res.render('camera'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
